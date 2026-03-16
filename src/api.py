d755719d7d7336e842a500354a1061290d61d5f5
unknown <mingzhuxin35@gmail.com>
2026-03-16 21:56:18 +0800

    update

--- src/api.py
+++ src/api.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 
-# 创建API
 app = FastAPI()
 
 # 加载模型
-model = joblib.load("../models/random_forest_model.pkl")
+model = joblib.load("models/random_forest_model.pkl")
+
+# 加载训练特征
+feature_columns = joblib.load("models/feature_columns.pkl")
 
 
 @app.get("/")
@@ -17,12 +19,19 @@ def home():
 @app.post("/predict")
 def predict(data: dict):
 
-    # 将输入转换为DataFrame
-    df = pd.DataFrame([data])
+    # 用户输入
+    input_df = pd.DataFrame([data])
 
-    # 模型预测
+    # 创建完整特征表
+    df = pd.DataFrame(columns=feature_columns)
+
+    # 填充输入
+    for col in input_df.columns:
+        if col in df.columns:
+            df[col] = input_df[col]
+
+    df = df.fillna(0)
+
     prediction = model.predict(df)[0]
 
-    return {
-        "predicted_price": float(prediction)
-    }
+    return {"predicted_price": float(prediction)}
